573bbf512b8b27390e24c2eb8851ab45deb3839b
Xu Yao <44374371+Hyacinth-YX@users.noreply.github.com>
2025-06-03 23:10:38 +0800

    修改了部分依赖版本，修改了ignore文件 (#1)

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 conda create -n ad python=3.9 -y
 conda activate ad
 
-conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+# conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 # pip install tensorflow
-pip3 install torch torchvision torchaudio
-pip install tf-nightly
+pip3 install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1
+pip install tf-nightly==2.20.0.dev20250327
 pip install -r requirements.txt
-pip install pytorchvideo
+pip install pytorchvideo==0.1.5
 # pip install git+https://github.com/facebookresearch/pytorchvideo.git
-pip install opencv-python
+pip install opencv-python==4.11.0.86
 # -i https://pypi.tuna.tsinghua.edu.cn/simple
