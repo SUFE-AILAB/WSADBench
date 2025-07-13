4d7d14855b7f25b09188108e01f5f3aa695eea1f
yaoxu <clearbamboo@outlook.com>
2025-07-13 21:05:50 +0800

    修正PReNet GPU运行代码的部分bug

--- WSADBench/baseline/PReNet/run.py
+++ WSADBench/baseline/PReNet/run.py
@@ -70,11 +70,11 @@ class PReNet():
             X_train_u_tensor = self.X_train_tensor[index_u]
 
             with torch.no_grad():
-                score_a_x = self.model(X_train_a_tensor, torch.cat(num * [X[i].view(1, -1)]))
-                score_x_u = self.model(torch.cat(num * [X[i].view(1, -1)]), X_train_u_tensor)
+                score_a_x = self.model(X_train_a_tensor.to(self.device), torch.cat(num * [X[i].view(1, -1)]).to(self.device))
+                score_x_u = self.model(torch.cat(num * [X[i].view(1, -1)]).to(self.device), X_train_u_tensor.to(self.device))
 
             score_sub = torch.mean(score_a_x + score_x_u)
-            score_sub = score_sub.numpy()[()]
+            score_sub = score_sub.cpu().numpy()[()]
 
             # entire score
             score.append(score_sub)
